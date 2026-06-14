78a009ee9991e572af9cf82714c6ae194dfdea7c
Jyoti27-ops <burade.jyoti27@gmail.com>
2026-06-14 17:20:11 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        while(true) {
+        while (true) {
             System.out.println("Application Running...");
             Thread.sleep(10000);
         }
